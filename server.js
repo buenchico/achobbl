80354ae2e09bf618f06c4c3ff6e332fb472d24ad
beunchico <hammer_ortiz@hotmail.com>
2025-11-28 12:45:20 +0000

    Fresh start

---
+++ server.js
@@ -0,0 +1,46 @@
+require('dotenv').config();
+
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+// Serve static files (CSS, images, etc.)
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Set up EJS as the templating engine
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views')); // Ensure your views are stored in the 'views' folder
+
+app.get("/script.js", (req, res) => {
+  res.setHeader("Content-Type", "application/javascript");
+  res.render("script", { environment: process.env.ENVIRONMENT });
+});
+
+// Define routes that render different "main" content
+app.get('/', (req, res) => {
+    res.render('application', { partial: 'fixtures' });
+});
+
+app.get('/:partial', (req, res) => {
+    const { partial } = req.params;
+    res.render('application', { partial });
+});
+
+// Dynamic route
+const teams = require('./public/teams.json'); // Load teams JSON once
+
+app.get('/teams/:url', (req, res) => {
+  const teamUrl = req.params.url;
+  const team = teams.find(t => t.url === teamUrl);
+
+  if (!team) {
+    return res.status(404).send('Team not found');
+  }
+
+  // Render teams.ejs partial with the correct gid
+  res.render('application', { partial: 'team', team });
+});
+
+const PORT = process.env.PORT;
+app.listen(PORT, () => console.log(`Server running at http://localhost:${PORT}/`));
